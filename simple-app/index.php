37f197034cfab0713ba324bc5058ea8167278595
ISkliarov <serhii.skliarov@intellias.com>
2021-09-24 14:33:01 +0300

    Add deployment mywebb

--- simple-app/index.php
+++ simple-app/index.php
@@ -3,6 +3,6 @@ $ip_server = $_SERVER['SERVER_ADDR'];
 
 echo "<h1>Hello from Kubernets</h1><br>";
 echo "Server IP Adress is: $ip_server", "<br><p>";
-echo "Made by <font color=blue> Serhii Skliarov";
+echo "Made by <font color=blue> Serhii Skliarov. <br> Version: 1.2 <br>";
 
 ?>
